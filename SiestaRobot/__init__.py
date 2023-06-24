657ebd06df66183cc0e301182d4791b6272ed761
LeaveEx <126212388+LeaveEx@users.noreply.github.com>
2023-06-24 07:24:38 +0700

    Update __init__.py

--- SiestaRobot/__init__.py
+++ SiestaRobot/__init__.py
@@ -197,7 +197,6 @@ else:
     INFOPIC = Config.INFOPIC
     BOT_USERNAME = Config.BOT_USERNAME
     STRING_SESSION = Config.STRING_SESSION
-    CF_API_KEY = Config.CF_API_KEY
 
     try:
         BL_CHATS = {int(x) for x in Config.BL_CHATS or []}
